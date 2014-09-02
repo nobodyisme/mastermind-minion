fc1230c80b2a5c481f6ecd87f2dc9def8475cfef
Andrey Vasilenkov <nobody.eie@gmail.com>
2014-09-02 19:30:06 +0400

    Fix for dnet ioclient subprocesses

--- src/minion/subprocess/dnet_ioclient.py
+++ src/minion/subprocess/dnet_ioclient.py
@@ -10,8 +10,8 @@ class DnetIoclientSubprocess(BaseSubprocess):
         super(DnetIoclientSubprocess, self).__init__(cmd, params=params, io_loop=io_loop)
         if not 'node' in params and not 'node_backend' in params:
             raise ValueError('Parameter "node" or "node_backend" is required')
-        self.node = params['node']
-        self.node_backend = params['node_backend']
+        self.node = params.get('node')
+        self.node_backend = params.get('node_backend')
 
     def watch(self):
         return ProgressWatcher(self.process)
