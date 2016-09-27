fc5a821cb3aed0c63fa53f60e06c11b9f87c50cb
Andrey Vasilenkov <indigo@yandex-team.ru>
2016-09-27 15:00:42 +0300

    Remove unused code

--- src/minion/watchers/dnet_client.py
+++ src/minion/watchers/dnet_client.py
@@ -6,36 +6,6 @@ from minion.watchers.base import ProgressWatcher
 
 class DnetClientWatcher(ProgressWatcher):
 
-    def is_success(self):
-
-        logger.info('pid {0}: exit code {1}'.format(
-            self.subprocess.pid, self.exit_code))
-
-        if self.exit_code == 0:
-            return True
-
-        output = ''.join(self.output)
-        try:
-            data = json.loads(output)
-        except Exception as e:
-            logger.error('pid {0}: failed to parse output json: {1}'.format(
-                self.subprocess.pid, output))
-            return False
-
-        if not 'error' in data:
-            logger.error('pid {0}: no "error" key in response data'.format(
-                self.subprocess.pid))
-            return False
-        if not 'code' in data['error']:
-            logger.error('pid {0}: no "code" key in response error data'.format(
-                self.subprocess.pid))
-            return False
-
-        logger.info('pid {0}: operation error code {1}'.format(
-            self.subprocess.pid, data['error']['code']))
-
-        return data['error']['code'] in self.success_codes
-
     def _command_code(self):
         if self.exit_code == 0:
             return self.exit_code
