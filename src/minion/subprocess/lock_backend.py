a93317ee71a47025e75242aaea818ac02b35720a
Evgenii Gorinov <jonny-k@yandex-team.ru>
2017-10-09 19:42:59 +0300

    Allow to write holder id to locks

--- src/minion/subprocess/lock_backend.py
+++ src/minion/subprocess/lock_backend.py
@@ -5,12 +5,19 @@ from minion.subprocess.base import BaseCommand
 class LockBackendCommand(BaseCommand):
 
     COMMAND = 'lock_backend'
-    REQUIRED_PARAMS = ('mark_backend',)
+    REQUIRED_PARAMS = (
+        'mark_backend',
+        # 'holder_id',  # temporary not required, uncomment this param later
+    )
 
     def execute(self):
+        holder_id = self.params.get('holder_id', '')
         marker = self.params['mark_backend']
+
         try:
-            open(marker, 'w').close()
+            with open(marker, 'w') as f:
+                f.write(holder_id)
+
         except Exception as e:
             cmd_logger.error('Failed to create backend marker: {}'.format(e), extra=self.log_extra)
             raise
