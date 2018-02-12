461ff7719778710e1608464528d9a8ed0eb0cc8f
Evgenii Gorinov <jonny-k@yandex-team.ru>
2018-02-12 18:21:57 +0300

    Account unavailable_groups to artifacts of recovery job

--- src/minion/subprocess/dnet_recovery.py
+++ src/minion/subprocess/dnet_recovery.py
@@ -29,30 +29,37 @@ class DnetRecoverySubprocess(BaseSubprocess):
 
         cmd_logger.info('Parsing commands stats path: {}'.format(commands_stats_path), extra=self.log_extra)
 
-        commands_stats = {}
+        job_stats = {}
 
         try:
             with open(commands_stats_path, 'rb') as f:
-                commands_stats = json.load(f).get('commands', {})
+                job_stats = json.load(f)
         except Exception:
             cmd_logger.exception(
                 'Failed to parse commands stats file {}'.format(commands_stats_path),
                 extra=self.log_extra,
             )
 
-        parsed_stats = self._parse_commands_stats(commands_stats)
+        parsed_stats = self._parse_job_stats(job_stats)
 
         # NOTE: temporary backward compatibility
         self.commands_stats = parsed_stats
         return parsed_stats
 
-    def _parse_commands_stats(self, commands_stats):
+    def _parse_job_stats(self, job_stats):
         op_statuses_count = {}
+
+        commands_stats = job_stats.get('commands', {})
         for operation_status, count in commands_stats.iteritems():
             operation, status = operation_status.split('.', 1)
             statuses_count = op_statuses_count.setdefault(operation, {})
             statuses_count.setdefault(status, 0)
             statuses_count[status] += count
+
+        unavailable_groups = job_stats.get('unavailable_groups')
+        if unavailable_groups is not None:
+            # we imitate artifacts format for such errors
+            op_statuses_count.setdefault('unavailable_groups', {})["-6"] = unavailable_groups
 
         cmd_logger.info('Parsed command statuses: {}'.format(op_statuses_count), extra=self.log_extra)
 
